119611eeb1e5e1c33ee2de18a22129d177d68161
Danyil Kastornyi <kastorniy.daniil@gmail.com>
2018-03-29 02:20:13 +0300

    Workers view refactoring
    Add Delete button. Create edit buttons with asociated worker modals.

--- resources/views/workers.blade.php
+++ resources/views/workers.blade.php
@@ -13,14 +13,14 @@
 </head>
 <body>
 
-    <div class="container mt-2">
+    <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        Workers
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h4>Workers list</h4>
                     </div>
-                    <div class="card-body">
+                    <div class="panel-body">
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="col-md-6">
@@ -61,10 +61,60 @@
                                         <td>{{ $worker->god_rozhdeniya }}</td>
                                         <td>{{ $worker->dolzhnost }}</td>
                                         <td>{{ $worker->zp_v_god }}</td>
+                                        <td>
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <button type="submit" class="btn btn-success btn-xs" data-toggle="modal" data-target="#myModal{{ $worker->id }}">
+                                                        <i class="fa fa-edit"></i>
+                                                    </button>
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <form  method="POST" action="{{action('WorkersController@removeWorker', $worker->id)}}">
+                                                        <input name="_method" type="hidden" value="DELETE">
+                                                        {{csrf_field()}}
+                                                        <button class="btn btn-danger btn-xs" type="submit">
+                                                            <i class="fa fa-trash"></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </td>
                                     </tr>
+
+                                    {{-- WORKERS MODALS --}}
+                                    <div class="modal fade" id="myModal{{ $worker->id }}" tabindex="-1" role="dialog">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                                    <h4 class="modal-title">Edit info</h4>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <input type="text" class="form-group form-control" name="famimliya" value="{{ $worker->famimliya }}">
+                                                    <input type="text" class="form-group form-control" name="imya" value="{{ $worker->imya }}">
+                                                    <input type="text" class="form-group form-control" name="otchestvo" value="{{ $worker->otchestvo }}">
+                                                    <input type="text" class="form-group form-control" name="god_rozhdeniya" value="{{ $worker->god_rozhdeniya }}">
+                                                    <input type="text" class="form-group form-control" name="dolzhnost" value="{{ $worker->dolzhnost }}">
+                                                    <input type="text" class="form-group form-control" name="zp_v_god" value="{{ $worker->zp_v_god }}">
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                    <form method="POST" action="{{ action('WorkersController@saveWorker') }}" class="form-row">
+                                                        {{ csrf_field() }}
+                                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    {{-- ENDWORKERS MODALS --}}
                                 @endforeach
                                 </tbody>
                             </table>
+                        </div>
+
+                        <div class="col-md-12">
+                            <a href="{{ URL::to('export-workers') }}" class="btn btn-dark pull-right"><button class="btn btn-info">Export file</button></a>
                         </div>
                     </div>
                 </div>
@@ -72,5 +122,6 @@
         </div>
     </div>
 
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
