29d5d5e1f130d2e029416e9779c805f66a05ee14
Danyil Kastornyi <kastorniy.daniil@gmail.com>
2018-03-29 10:25:52 +0300

    Comments refactoring. Move create worker modal to bottom

--- resources/views/workers.blade.php
+++ resources/views/workers.blade.php
@@ -6,13 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Workers list</title>
-    {{-- STYLES --}}
+    <!-- STYLES -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    {{-- END STYLES --}}
+    <!-- END STYLES -->
 </head>
 <body>
-
+    <!-- CONTENT -->
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -24,6 +24,7 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="col-md-6">
+                                    <!-- IMPORT WORKER FORM -->
                                     <form method="POST" action="{{ action('WorkersController@importWorkers') }}" enctype="multipart/form-data" class="form-row">
                                         {{ csrf_field() }}
                                         <div class="col-md-10">
@@ -37,47 +38,18 @@
                                             <button type="submit" class="btn btn-primary">Import file</button>
                                         </div>
                                     </form>
+                                    <!-- END IMPORT WORKER FORM -->
                                 </div>
                                 <div class="col-md-2">
+                                    <!-- CREATE WORKER -->
                                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#create-worker-modal">
                                         Create new worker
                                     </button>
-
-                                    <div class="modal fade" id="create-worker-modal" tabindex="-1" role="dialog">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                        <span aria-hidden="true">&times;</span>
-                                                    </button>
-                                                    <h4 class="modal-title">Create new worker</h4>
-                                                </div>
-                                                <form method="POST" action="{{ action('WorkersController@createWorker') }}" class="form-row">
-                                                    <div class="modal-body">
-                                                            {{ csrf_field() }}
-
-                                                            <input type="text" class="form-group form-control" name="famimliya" placeholder="Фамилия">
-                                                            <input type="text" class="form-group form-control" name="imya" placeholder="Имя">
-                                                            <input type="text" class="form-group form-control" name="otchestvo" placeholder="Отчество">
-                                                            <input type="text" class="form-group form-control" name="god_rozhdeniya" placeholder="Год рождения">
-                                                            <input type="text" class="form-group form-control" name="dolzhnost" placeholder="Должность">
-                                                            <input type="text" class="form-group form-control" name="zp_v_god" placeholder="Зп в год">
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-default" data-dismiss="modal">
-                                                            Close
-                                                        </button>
-                                                        <button type="submit" class="btn btn-primary">
-                                                            Save changes
-                                                        </button>
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <!-- END CREATE WORKER -->
                                 </div>
                             </div>
                         </div>
+
                         <br>
 
                         <div class="col-md-12">
@@ -110,6 +82,7 @@
                                                     </button>
                                                 </div>
                                                 <div class="col-md-3">
+                                                    <!-- REMOVE WORKER FORM -->
                                                     <form  method="POST" action="{{ action('WorkersController@removeWorker', $worker->id) }}">
                                                         {{ csrf_field() }}
                                                         {{ method_field('DELETE') }}
@@ -117,12 +90,13 @@
                                                             <i class="fa fa-trash"></i>
                                                         </button>
                                                     </form>
+                                                    <!-- END REMOVE WORKER FORM -->
                                                 </div>
                                             </div>
                                         </td>
                                     </tr>
 
-                                    {{-- WORKERS MODALS --}}
+                                    <!-- WORKERS MODALS -->
                                     <div class="modal fade" id="worker-{{ $worker->id }}-modal" tabindex="-1" role="dialog">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
@@ -132,6 +106,7 @@
                                                     </button>
                                                     <h4 class="modal-title">Edit info</h4>
                                                 </div>
+                                                <!-- EDIT WORKER FORM -->
                                                 <form method="POST" action="{{ action('WorkersController@editWorker', $worker->id) }}" class="form-row">
                                                     <div class="modal-body">
                                                             {{ csrf_field() }}
@@ -153,29 +128,70 @@
                                                         </button>
                                                     </div>
                                                 </form>
+                                                <!-- END EDIT WORKER FORM -->
                                             </div>
                                         </div>
                                     </div>
-                                    {{-- END WORKERS MODALS --}}
+                                    <!-- END WORKERS MODALS -->
                                 @endforeach
                                 </tbody>
                             </table>
                         </div>
 
                         <div class="col-md-12">
+                            <!-- EXPORT WORKERS -->
                             <a href="{{ url('export-workers') }}" class="btn btn-dark pull-right">
                                 <button class="btn btn-primary">
                                     Export file
                                 </button>
                             </a>
+                            <!-- END EXPORT WORKERS -->
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- END CONTENT -->
 
+    <!-- CREATE WORKER MODAL -->
+    <div class="modal fade" id="create-worker-modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Create new worker</h4>
+                </div>
+                <form method="POST" action="{{ action('WorkersController@createWorker') }}" class="form-row">
+                    <div class="modal-body">
+                        {{ csrf_field() }}
+
+                        <input type="text" class="form-group form-control" name="famimliya" placeholder="Фамилия">
+                        <input type="text" class="form-group form-control" name="imya" placeholder="Имя">
+                        <input type="text" class="form-group form-control" name="otchestvo" placeholder="Отчество">
+                        <input type="text" class="form-group form-control" name="god_rozhdeniya" placeholder="Год рождения">
+                        <input type="text" class="form-group form-control" name="dolzhnost" placeholder="Должность">
+                        <input type="text" class="form-group form-control" name="zp_v_god" placeholder="Зп в год">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">
+                            Close
+                        </button>
+                        <button type="submit" class="btn btn-primary">
+                            Save changes
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- END CREATE WORKER MODAL -->
+
+    <!-- SCRIPTS -->
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/custom.js') }}"></script>
+    <!-- END SCRIPTS -->
 </body>
 </html>
